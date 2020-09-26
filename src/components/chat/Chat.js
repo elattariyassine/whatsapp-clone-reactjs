eb19c99b8dd595ab82dbb01677e8be04c5c0bf55
ELATTARIYassine <yassineattari64@gmail.com>
2020-09-26 16:33:16 +0000

    Added last seen at

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -55,7 +55,12 @@ const Chat = () => {
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
           <h3>{roomName ? roomName : "Getting the name..."}</h3>
-          <p>Last seen at ...</p>
+          <p>
+            last seen{" "}
+            {new Date(
+              messages[messages.length - 1]?.timestamp?.toDate()
+            ).toUTCString()}
+          </p>
         </div>
         <div className="chat__headerRight">
           <IconButton>
