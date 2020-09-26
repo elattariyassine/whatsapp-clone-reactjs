a3522ae9607e7841aacd802bf2fb94fcb3d9d336
ELATTARIYassine <yassineattari64@gmail.com>
2020-09-26 14:20:42 +0000

    Added login component

--- src/App.js
+++ src/App.js
@@ -3,24 +3,31 @@ import "./App.css";
 import SideBar from "./components/sidebar/SideBar";
 import Chat from "./components/chat/Chat";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { useState } from "react";
+import Login from "./components/login/Login";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <SideBar />
-          <Switch>
-            <Route path="/rooms/:roomId">
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <SideBar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <h1>Hello</h1>
+              </Route>
               <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Hello</h1>
-            </Route>
-            <Chat />
-          </Switch>
-        </Router>
-      </div>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
